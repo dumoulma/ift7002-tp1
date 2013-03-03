774de7e09fa538022a4b509939118bd83a77f64a
Mathieu Dumoulin <mathieu.dumoulin@gmail.com>
2013-03-02 22:52:21 -0500

    various fix

--- TP1/confidence_intervals.py
+++ TP1/confidence_intervals.py
@@ -6,34 +6,31 @@ Created on Mar 2, 2013
 import numpy as np
 from math import log, sqrt
 from functools import reduce
-from rpy import *
 
-def __factorial(n): 
+def factorial(n): 
     if n < 2: return 1
     return reduce(lambda x, y: x * y, range(2, int(n) + 1))
 
-def __binomial(n, k, r):
+def binomial(n, k, r):
     prob = 0
     k = int(k)
     for i in range(0, k + 1):
-        prob += __factorial(n) / (__factorial(k) * __factorial(n - k)) * r ** i * (1 - r) ** (n - i)
+        prob += factorial(n) / (factorial(k) * factorial(n - k)) * r ** i * (1 - r) ** (n - i)
     return prob
 
-def __invbin(n, nRz, delta):
-    r_max = 0
-    r_range = np.arange(0, nRz, nRz / 100)
+def invbin(n, nRz, delta):
     i = 0
-    prob = rpy.pbinom(n, nRz, r) 
+    r_range = np.arange(0,nRz,nRz/100)
+    prob = binomial(n, nRz, r_range[i])
     while prob >= delta and i < r_range.size:
-        r_val = r_range[i]
-        r_max = r_val
-        i += 1
+        i += 1        
+        prob = binomial(n, nRz, r_range[i])
         
-    return r_max
+    return r_range[i]
 
 def confint_binomial(n, k, delta):
-    lower = __invbin(n, k, 1 - delta)
-    upper = __invbin(n, k, delta)
+    lower = invbin(n, k, 1 - delta)
+    upper = invbin(n, k, delta)
     return lower, upper
 
 z_table = {0.5:0.67, 0.68:1, 0.80:1.28, 0.90:1.64, 0.95:1.96, 0.98:2.33, 0.99:2.58}
@@ -45,3 +42,8 @@ def confint_hoeffding(n, delta):
     delta = 0.1
     return sqrt((1 / (2 * n)) * log(2 / delta))
     
+n = 500
+k = 25
+delta = 0.1
+u,l = confint_binomial(n,k,delta)
+print("Confint=[%0.5f,%0.5f]"%(l,u))
